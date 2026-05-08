0ab899912ef0c5d0f0bbd892eb20cf131316d785
camilo <cc.leiva@duocuc.cl>
2026-05-08 04:54:06 -0400

    jwt y cambios en el pomxml

---
+++ src/main/java/com/Inovatech/mslogin/service/JwtService.java
@@ -0,0 +1,29 @@
+package com.Inovatech.mslogin.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private final String SECRET_KEY =
+            "mi_clave_super_secreta_para_microservicios";
+
+    public String generateToken(String email) {
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(
+                        new Date(System.currentTimeMillis() + 86400000)
+                )
+                .signWith(
+                        SignatureAlgorithm.HS256,
+                        SECRET_KEY
+                )
+                .compact();
+    }
+}
